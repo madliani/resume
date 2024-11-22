7c1ed6fd9c7e84f86d9dd2f38582099212efedc1
madliani <madliani@hotmail.com>
2024-11-22 16:09:19 +0400

    fix: fix education card styles

--- src/components/EduCard.tsx
+++ src/components/EduCard.tsx
@@ -9,7 +9,7 @@ export const EduCard = ({ className }: Properties) => (
     <Card className={className}>
         <CardHeader data-testid="edu-card__title">Education</CardHeader>
         <Divider />
-        <CardBody class="flex items-baseline justify-around">
+        <CardBody class="flex items-baseline justify-center mt-8 mb-8">
             <Card>
                 <CardHeader data-testid="edu-card__text">
                     Petrozavodsk State University
@@ -22,7 +22,7 @@ export const EduCard = ({ className }: Properties) => (
                 <CardFooter>09/2018 &mdash; 06/2020</CardFooter>
             </Card>
 
-            <Card className="mt-4">
+            <Card className="ml-8">
                 <CardHeader>Petrozavodsk State University</CardHeader>
                 <Divider />
                 <CardBody>
