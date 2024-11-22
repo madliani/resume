5725d49e2d6bab05dc19fcb0efde8b079b270895
madliani <madliani@hotmail.com>
2024-11-22 16:21:19 +0400

    feat: add photo

---
+++ src/components/Photo.tsx
@@ -0,0 +1,16 @@
+import { Image } from "@nextui-org/image";
+import SocialAvatar from "../assets/images/social_avatar.jpeg";
+
+type Properties = {
+    className?: React.CSSProperties;
+};
+
+export const Photo = ({ className }: Properties) => (
+    <Image
+        className={className}
+        src={SocialAvatar}
+        alt="Me"
+        width={340}
+        height={480}
+    />
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { AboutCard } from "../components/AboutCard";
 import { EduCard } from "../components/EduCard";
 import { ExpCard } from "../components/ExpCard";
+import { Photo } from "../components/Photo";
 import { SEO } from "../components/SEO";
 import { Layout } from "../layouts/Layout";
 import { Main } from "../layouts/Main";
@@ -11,7 +12,8 @@ export const Home = () => {
             <SEO title="Home" />
             <Main className="flex flex-wrap">
                 <section className="mt-8 w-full flex flex-wrap items-center justify-center">
-                    <AboutCard className="w-2/3" />
+                    <Photo />
+                    <AboutCard className="w-1/3 ml-8" />
                 </section>
                 <section className="mt-8 w-full flex flex-col flex-wrap items-center justify-center">
                     <EduCard className="w-2/3" />
