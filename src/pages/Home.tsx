aab51a177f2a6db81e135fc63b56419499bae5cf
madliani <madliani@hotmail.com>
2024-11-23 20:03:16 +0400

    fix: fix homepage styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ export const Home = () => {
         <Layout>
             <SEO title="Home" />
             <Main className="flex flex-col flex-wrap">
-                <section className="mt-8 w-full flex flex-wrap items-center justify-center">
+                <section className="mt-8 w-full flex flex-col flex-wrap items-center justify-center tablet:flex-row tablet:items-center tablet:justify-center">
                     <Photo className="w-[192px] h-[256px] tablet:w-[360px] tablet:h-[480px]" />
                     <AboutCard className="w-11/12 mt-8 tablet:w-5/12 tablet:ml-8" />
                 </section>
