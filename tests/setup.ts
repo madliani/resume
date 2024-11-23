0585a2ddec7102ab5be378d776a9e4422f06545f
madliani <madliani@hotmail.com>
2024-11-23 12:20:46 +0400

    feat: add jest-extended

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,5 @@
 import { afterEach } from "@jest/globals";
-import "@testing-library/jest-dom/jest-globals";
 import { cleanup } from "@testing-library/react";
+import "jest-extended/all";
 
 afterEach(() => cleanup());
