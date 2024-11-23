3a1f3d19d09762d1311494be695b8879e86c8d22
madliani <madliani@hotmail.com>
2024-11-23 22:25:53 +0400

    feat: replace photo by image

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
-import { Photo } from "./Photo";
+import { Image } from "@nextui-org/image";
+import SocialAvatar from "../assets/images/social_avatar.jpeg";
 
 type Properties = {
     className?: React.CSSProperties;
@@ -9,7 +10,7 @@ type Properties = {
 export const PhotoCard = ({ className }: Properties) => (
     <Card className={className}>
         <CardHeader>
-            <Photo className="w-[192px] h-[256px] tablet:w-[360px] tablet:h-[480px]" />
+            <Image className={className} src={SocialAvatar} alt="Me" />
         </CardHeader>
         <CardBody>
             <Button color="primary">
