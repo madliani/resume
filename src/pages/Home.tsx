0682420a16397d296beda0c039b483e7434620c5
madliani <madliani@hotmail.com>
2024-11-23 19:59:10 +0400

    fix: fix homepage styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ export const Home = () => {
     return (
         <Layout>
             <SEO title="Home" />
-            <Main className="flex flex-wrap">
+            <Main className="flex flex-col flex-wrap">
                 <section className="mt-8 w-full flex flex-wrap items-center justify-center">
                     <Photo className="w-[192px] h-[256px] tablet:w-[360px] tablet:h-[480px]" />
                     <AboutCard className="w-11/12 mt-8 tablet:w-5/12 tablet:ml-8" />
