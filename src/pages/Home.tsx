88876b9ce6dd7278cb97c56e7e43df32717bba4a
madliani <madliani@hotmail.com>
2024-11-23 20:18:52 +0400

    fix: fix homepage styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ export const Home = () => {
                 </section>
                 <section className="mt-8 mb-8 w-full flex flex-col flex-wrap items-center justify-center">
                     <EduCard className="w-11/12 tablet:w-8/12" />
-                    <ExpCard className="w-11/12 tablet:w-8/12 tablet:mt-8" />
+                    <ExpCard className="w-11/12 mt-8 tablet:w-8/12" />
                 </section>
             </Main>
         </Layout>
