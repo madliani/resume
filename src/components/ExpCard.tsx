0ec088efad4a00afb8ccd1d2d8515257ec665a5b
madliani <madliani@hotmail.com>
2025-02-26 22:36:29 +0400

    fix: change experience card

--- src/components/ExpCard.tsx
+++ src/components/ExpCard.tsx
@@ -11,50 +11,37 @@ export const ExpCard = ({ className }: Properties) => (
         <Divider />
         <CardBody class="flex flex-col flex-wrap items-center justify-center mt-8 mb-8">
             <Card className="w-11/12">
-                <CardHeader data-testid="exp-card__text">inter://so</CardHeader>
-                <Divider />
-                <CardBody>
-                    <p>
-                        At inter://so, I led the front-end development efforts
-                        for a client-facing application. I used Figma designs
-                        and integrated Google Maps and REST APIs to bring the
-                        project to life. I was responsible for designing the
-                        project's architecture, prioritizing scalable solutions
-                        that improved performance and user experience.
-                        Throughout the development process, I worked closely
-                        with designers and backend developers to ensure smooth
-                        integration and functionality, resulting in the
-                        successful delivery of a robust and responsive
-                        application.
-                    </p>
-                </CardBody>
-                <Divider />
-                <CardFooter>02/2022 &mdash; 09/2022</CardFooter>
-            </Card>
-
-            <Card className="w-11/12 mt-8">
                 <CardHeader>axmit</CardHeader>
                 <Divider />
                 <CardBody>
                     <p>
-                        At axmit, my role as a React developer spanned several
-                        outsourcing projects where I effectively applied my
-                        expertise. I focused on integrating and configuring
-                        Google Analytics to monitor user interactions and
-                        optimize website performance. Working closely with
-                        design teams, I used Figma to ensure project alignment
-                        with client goals and expectations. I also enhanced
-                        location-based functionality by integrating the Google
-                        Maps API and improved data retrieval efficiency by
-                        integrating the REST API for dynamic content.
-                        Implementation of OpenGraph meta tags further increased
-                        social media sharing and SEO visibility. Throughout
-                        these initiatives, I maintained proactive collaboration
-                        with QA teams to address issues, resolve bugs, and
-                        introduce new features in a timely manner. This approach
-                        ensured the delivery of quality solutions and high
-                        customer satisfaction.
+                        While working as a front-end developer, I implemented
+                        many improvements for several websites that positively
+                        impacted the user experience.
                     </p>
+                    <p>Improvements implemented:</p>
+                    <ul>
+                        <li>
+                            Integrated and configured Google Analytics to track
+                            user interaction with the interface and optimize
+                            application performance.
+                        </li>
+                        <li>
+                            Integrated Google Maps API to select relevant
+                            content based on user location.
+                        </li>
+                        <li>
+                            Added OpenGraph meta tags to increase social media
+                            conversions and improve SEO metrics.
+                        </li>
+                    </ul>
+                    <p>Stack used:</p>
+                    <ul>
+                        <li>React</li>
+                        <li>Redux and Redux-Saga</li>
+                        <li>JavaScript and TypeScript</li>
+                        <li>SCSS</li>
+                    </ul>
                 </CardBody>
                 <Divider />
                 <CardFooter>06/2019 &mdash; 10/2020</CardFooter>
