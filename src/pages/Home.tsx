d67cd01535a65cfebfabb7075d79f5b6da450ed6
madliani <madliani@hotmail.com>
2025-03-03 17:13:13 +0400

    fix: remove photo card from homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { AboutCard } from "../components/AboutCard";
 import { EduCard } from "../components/EduCard";
 import { ExpCard } from "../components/ExpCard";
-import { PhotoCard } from "../components/PhotoCard";
 import { SEO } from "../components/SEO";
 import { Layout } from "../layouts/Layout";
 import { Main } from "../layouts/Main";
@@ -12,7 +11,6 @@ export const Home = () => {
             <SEO title="Home" />
             <Main className="flex flex-col flex-wrap">
                 <section className="mt-8 w-full flex flex-col flex-wrap items-center justify-center tablet:flex-row tablet:items-center tablet:justify-center">
-                    <PhotoCard />
                     <AboutCard className="w-11/12 mt-8 tablet:w-5/12 tablet:ml-8" />
                 </section>
                 <section className="mt-8 mb-8 w-full flex flex-col flex-wrap items-center justify-center">
